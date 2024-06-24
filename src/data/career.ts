48ac86bca1366a129eee2a2d713d9d622e5b9511
Malan Gunaratne <malan-g@outlook.com>
2024-06-24 14:03:09 +1200

    updated careers mock-data using copilot auto-complete

--- src/data/career.ts
+++ src/data/career.ts
@@ -16,6 +16,195 @@ export const career = [
             description:
               "I used JavaScript to write simple client-side logic to handle browser interactions. Some examples include handling user clicks and window resizes, as well as working with the Intersection Observer API. ",
           },
+          {
+            key: "skill-2",
+            title: "PHP",
+            description:
+              "I primarily used PHP to write server-side logic to load collections from Databases. I also used PHP to work with Wordpress themes & Plugins.",
+          },
+          {
+            key: "skill-3",
+            title: "Bash",
+            description:
+              "I used Bash to write simple scripts to automate tasks such as setting up new projects.",
+          },
+          {
+            key: "skill-4",
+            title: "SQL",
+            description:
+              "I used SQL to write queries to interact with databases. I used SQL to create, read, update, and delete data from databases.",
+          },
+        ],
+      },
+      {
+        key: "item-2",
+        title: "Markup and Style Languages",
+        skills: [
+          {
+            key: "skill-1",
+            title: "HTML",
+            description:
+              "I used HTML to write the structure of the websites I developed. I used HTML to create the layout of the websites I developed.",
+          },
+          {
+            key: "skill-2",
+            title: "CSS/SCSS",
+            description:
+              "I used CSS to style the websites I developed. I used CSS to create the visual design of the websites I developed.",
+          },
+          {
+            key: "skill-3",
+            title: "Tailwind CSS",
+            description:
+              "I used Tailwind CSS to style the websites I developed.",
+          },
+        ],
+      },
+      {
+        key: "item-3",
+        title: "JavaScript Frameworks and Libraries",
+        skills: [
+          {
+            key: "skill-1",
+            title: "Alpine.js",
+            description:
+              "I used Alpine.js to write simple client-side logic to handle browser interactions. Some examples include handling user clicks and window resizes, as well as working with the Intersection Observer API.",
+          },
+          {
+            key: "skill-2",
+            title: "Swiper.js",
+            description: "I used Swiper.js to create carousels on websites.",
+          },
+          {
+            key: "skill-3",
+            title: "Three.js",
+            description: "I used Three.js to create 3D animations on websites.",
+          },
+          {
+            key: "skill-4",
+            title: "Google Maps",
+            description:
+              "I used the Google Maps API to create interactive maps on websites.",
+          },
+        ],
+      },
+      {
+        key: "item-4",
+        title: "PHP Frameworks and Libraries",
+        skills: [
+          {
+            key: "skill-1",
+            title: "Laravel",
+            description:
+              "Laravel is a PHP framework for building web applications. I used Laravel to build websites using the MVC pattern.",
+          },
+          {
+            key: "skill-2",
+            title: "Laravel Livewire",
+            description:
+              "Laravel Livewire is a library for building dynamic interfaces in Laravel. I used Laravel Livewire to create dynamic interfaces on websites.",
+          },
+          {
+            key: "skill-3",
+            title: "Laravel Valet",
+            description:
+              "Laravel Valet is a development environment for Laravel.",
+          },
+          {
+            key: "skill-4",
+            title: "Laravel Mix",
+            description:
+              "Laravel Mix is a tool for compiling assets in Laravel.",
+          },
+          {
+            key: "skill-5",
+            title: "Eloquent ORM",
+            description:
+              "Eloquent ORM is an object-relational mapping library for Laravel.",
+          },
+          {
+            key: "skill-6",
+            title: "Filament PHP",
+            description:
+              "Filament PHP is a library for building admin panels in Laravel.",
+          },
+        ],
+      },
+      {
+        key: "item-5",
+        title: "Content Management Systems",
+        skills: [
+          {
+            key: "skill-1",
+            title: "Wordpress",
+            description:
+              "I used Wordpress to create custom themes and plugins for websites.",
+          },
+          {
+            key: "skill-2",
+            title: "Twill CMS",
+            description:
+              "I used Twill CMS to create custom themes and plugins for websites.",
+          },
+        ],
+      },
+      {
+        key: "item-6",
+        title: "E-commerce Platforms",
+        skills: [
+          {
+            key: "skill-1",
+            title: "Shopify",
+            description:
+              "I used Shopify to create custom themes and plugins for websites.",
+          },
+          {
+            key: "skill-2",
+            title: "WooCommerce",
+            description:
+              "I used WooCommerce to create custom themes and plugins for websites.",
+          },
+        ],
+      },
+      {
+        key: "item-7",
+        title: "Development Tools and Environment",
+        skills: [
+          {
+            key: "skill-1",
+            title: "VSCode",
+            description: "I used VSCode as my code editor.",
+          },
+          {
+            key: "skill-2",
+            title: "Git",
+            description: "I used Git for version control.",
+          },
+          {
+            key: "skill-3",
+            title: "Plesk cPanel",
+            description: "I used Plesk cPanel to manage servers.",
+          },
+          {
+            key: "skill-4",
+            title: "TablePlus",
+            description: "I used TablePlus to interact with databases.",
+          },
+          {
+            key: "skill-5",
+            title: "DBngin",
+            description: "I used DBngin to manage databases.",
+          },
+          {
+            key: "skill-6",
+            title: "Postman",
+            description: "I used Postman to test APIs.",
+          },
+          {
+            key: "skill-7",
+            title: "NVM",
+            description: "I used NVM to manage Node versions.",
+          },
         ],
       },
     ],
@@ -28,7 +217,7 @@ export const career = [
       "As part of my final year R&D project, our team partnered with Deloitte Digital & Auckland District Health Board to prototype an AI application",
     AccordionItems: [
       {
-        key: "item-2",
+        key: "item-1",
         title: "Programming Languages",
         skills: [
           {
@@ -36,6 +225,60 @@ export const career = [
             title: "React Native",
             description:
               "We used React Native to develop to deliver our prototype to both iOS and Android devices.",
+          },
+        ],
+      },
+      {
+        key: "item-2",
+        title: "Markup and Style Languages",
+        skills: [
+          {
+            key: "skill-1",
+            title: "CSS",
+            description: "Simple styles using React Natives StyleSheet API.",
+          },
+        ],
+      },
+      {
+        key: "item-3",
+        title: "Amazon Web Services",
+        skills: [
+          {
+            key: "skill-1",
+            title: "AWS Transcribe",
+            description:
+              "We used AWS Transcribe to transcribe audio recordings.",
+          },
+          {
+            key: "skill-2",
+            title: "AWS Lex",
+            description: "We used AWS Lex to create a chatbot.",
+          },
+        ],
+      },
+      {
+        key: "item-7",
+        title: "Development Tools and Environment",
+        skills: [
+          {
+            key: "skill-1",
+            title: "VSCode",
+            description: "I used VSCode as my code editor.",
+          },
+          {
+            key: "skill-2",
+            title: "BitBucket",
+            description: "I used BitBucket for version control.",
+          },
+          {
+            key: "skill-3",
+            title: "Trello",
+            description: "I used Trello to manage tasks.",
+          },
+          {
+            key: "skill-4",
+            title: "Miro",
+            description: "I used Miro to create user flows.",
           },
         ],
       },
